6872e96eb9386b3831231e20f846bb75862baf54
Francisco García <franciscogarciapozo@gmail.com>
2023-06-21 19:48:34 +0200

    3.5: Phonebook backend step5 Done

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const express = require('express')
 const app = express()
+
+app.use(express.json())
+
 const PORT = 3001
 
 let persons = [
     { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+      id: 1,
+      name: "Arto Hellas", 
+      number: "040-123456"
     },
     { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+      id: 2,
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
     },
     { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+      id: 3,
+      name: "Dan Abramov", 
+      number: "12-43-234345"
     },
     { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+      id: 4,
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
     }
 ]
 
@@ -60,6 +63,38 @@ app.delete('/api/persons/:id', (request, response) => {
 
     response.status(204).end()
 })
+
+const generateId = () => {
+    const randomId = Math.floor(Math.random() * 1000000)
+    return randomId
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    
+    if (!body.name) {
+      return response.status(400).json({ 
+        error: 'Name missing' 
+      })
+    }
+  
+    if (!body.number) {
+        return response.status(400).json({ 
+          error: 'Number missing' 
+        })
+      }
+    
+    const person = {
+      id: generateId(),
+      name: body.name,
+      number: body.number
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+  })
+
 
 app.listen(PORT, () => {
     console.log(`Server initialized in port ${PORT}`)
