9cfe7279a985362eb900ce462fd92b939104a817
Francisco García <franciscogarciapozo@gmail.com>
2023-06-21 19:18:56 +0200

    3.3: Phonebook backend step3 Done

--- index.js
+++ index.js
@@ -33,6 +33,18 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const person = persons.find(person => person.id === id)
+    if(person){
+        response.json(person)
+    } else {
+        response.statusMessage = "Can't find the person"
+        response.status(404).end()
+    }
+})
+
 app.get('/info', (request, response) => {
     let numberOfPersons = persons.length
     let currentHour = new Date();
