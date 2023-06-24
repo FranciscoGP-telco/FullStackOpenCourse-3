9d21a204acea51ffa27d6a2525f7ff0281fd0eec
Francisco García <franciscogarciapozo@gmail.com>
2023-06-24 15:40:17 +0200

    Exercises 3.13.-3.14 Done

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 
 morgan.token('body', function (req, res) { 
   return JSON.stringify(req.body)
@@ -15,47 +18,22 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors())
 app.use(express.json())
 
-
-let persons = [
-    { 
-      id: 1,
-      name: "Arto Hellas", 
-      number: "040-123456"
-    },
-    { 
-      id: 2,
-      name: "Ada Lovelace", 
-      number: "39-44-5323523"
-    },
-    { 
-      id: 3,
-      name: "Dan Abramov", 
-      number: "12-43-234345"
-    },
-    { 
-      id: 4,
-      name: "Mary Poppendieck", 
-      number: "39-23-6423122"
-    }
-]
-
 app.get('/', (request, response) => {
     response.send("<h1>Test for exercise 3.1</h1>")
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if(person){
-        response.json(person)
-    } else {
-        response.statusMessage = "Can't find the person"
-        response.status(404).end()
-    }
+  Person.findById(request.params.id)
+    .then(person => {
+      response.json(person)
+    })
 })
 
 app.get('/info', (request, response) => {
@@ -68,16 +46,11 @@ app.get('/info', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(person => {
+      response.json(person)
+    })
 })
-
-const generateId = () => {
-    const randomId = Math.floor(Math.random() * 1000000)
-    return randomId
-}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -93,23 +66,28 @@ app.post('/api/persons', (request, response) => {
           error: 'Number missing' 
         })
       }
-    
-      if (persons.find(person => person.name === body.name)){
-        return response.status(400).json({ 
-            error: 'Name already in the phonebook' 
+      
+    Person.find({name: body.name})
+      .then((personFind) => {
+        if(personFind.length === 0){
+          const person = new Person({
+            name: body.name,
+            number: body.number
+          })
+          
+          person.save()
+            .then(savedPerson => {
+              response.json(savedPerson)
+          })
+        } else {
+          return response.status(400).json({ 
+            error: 'Name duplicated in the DB' 
           })
         }
+      })
+    
 
-    const person = {
-      id: generateId(),
-      name: body.name,
-      number: body.number
-    }
-  
-    persons = persons.concat(person)
-  
-    response.json(person)
-  })
+})
 
 
 app.listen(PORT, () => {
