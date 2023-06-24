5c53a0f9707f313ddbe2379855c2e927b203d6c6
Francisco García <franciscogarciapozo@gmail.com>
2023-06-24 16:20:20 +0200

    3.16: Phonebook database, step4 Done

--- index.js
+++ index.js
@@ -5,18 +5,43 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
 
-const app = express()
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+
+const app = express()
 const PORT = process.env.PORT
 
 morgan.token('body', function (req, res) { 
   return JSON.stringify(req.body)
 })
 
+
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-//app.use(express.static('build'))
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
     response.send("<h1>Test for exercise 3.1</h1>")
@@ -29,34 +54,28 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
-      response.json(person)
+      if(person){
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-    let numberOfPersons = persons.length
-    let currentHour = new Date();
-    response.send(
-        `<p>Phonebook has info for ${numberOfPersons} people</p>
-        <p>${currentHour}</p>`
-    )
-})
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(person => {
       response.status(204).end()
     })
-    .catch(error => {
-      console.log(error)
-      response.status(500).end()
-    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     
     if (!body.name) {
@@ -89,10 +108,12 @@ app.post('/api/persons', (request, response) => {
           })
         }
       })
+      .catch(error => next(error))
     
-
 })
 
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server initialized in port ${PORT}`)
