82944baafaab0e5a6c50ec3905712161ec3c46ee
Francisco García <franciscogarciapozo@gmail.com>
2023-06-25 16:34:30 +0200

    3.19*: Phonebook database, step7 Done

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-
   next(error)
 }
 
@@ -113,7 +114,8 @@ app.post('/api/persons', (request, response, next) => {
           .then(savedPerson => {
             response.json(savedPerson)
         })
-      } else {
+        .catch(error => next(error))
+      } else {error
         return response.status(400).json({ 
           error: 'Name duplicated in the DB' 
         })
@@ -130,7 +132,11 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number
   }
 
-  Person.findByIdAndUpdate(request.params.id, person)
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    person,
+    { new: true, runValidators: true}
+  )
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
